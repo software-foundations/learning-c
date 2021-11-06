44e2aa029cd7c1f994bca29e019f89280394de7c
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-11-06 15:48:54 -0300

    (06_eequality_operators.c, at /01_basics) -> == and != usage

---
+++ 01_basics/06_equality_operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  // 1 if true
+  // 0 if false
+
+  printf("%d\n", 1 == 2);
+  printf("%d\n", 1 == 1);
+
+  printf("%d\n", 1 != 2);
+  printf("%d\n", 1 != 1);
+
+  return 0;
+}

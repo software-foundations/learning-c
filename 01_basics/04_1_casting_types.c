8bb5e2aef9bdb8c64803a47c5d18f276f333ea8d
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-11-06 18:50:40 -0300

    (04_1_casting_types.c, at /01_basics) -> sptrinf() to cast int of float to char

--- 01_basics/04_1_casting_types.c
+++ 01_basics/04_1_casting_types.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-  char text[1];
+  char string_from_int[2];
+  char string_from_float[10];
 
   printf("%d\n", (int)1.0);
   printf("%f\n", (float)1);
   printf("%.2f\n", (float)2);
+
+  sprintf(string_from_int, "%d", 3);
+  printf("%s\n", string_from_int);
+
+  sprintf(string_from_float, "%f", 4.0);
+  printf("%s\n", string_from_float);
 
   return 0;
 }

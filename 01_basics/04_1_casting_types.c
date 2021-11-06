2971406e133d4af157b341e67619785941483e98
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-11-06 18:41:00 -0300

    (04_1_casting_types.c, at /01_basics) -> casting between float and int

---
+++ 01_basics/04_1_casting_types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  char text[1];
+
+  printf("%d\n", (int)1.0);
+  printf("%f\n", (float)1);
+  printf("%.2f\n", (float)2);
+
+  return 0;
+}

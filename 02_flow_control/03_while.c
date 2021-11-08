32547c25c8ccbb145ea71ccda81ab0c5bd566a5b
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-11-08 14:06:26 -0300

    (03_while.c, at /02_flow_control/03_while.c) -> more while usage with var++ and --var like

--- 02_flow_control/03_while.c
+++ 02_flow_control/03_while.c
@@ -3,10 +3,37 @@
 
 int main(void) {
   int i = 0;
+  int j = 0;
+
+  int k = -1;
+  int l = 10;
+
+  // counter-controleld repetition
 
   while (i < 10) {
     printf("%d\n", i);
     i = i + 1;
+  }
+
+  printf("\n");
+
+  while (j < 10) {
+    printf("%d\n", j);
+    j++;
+  }
+
+  printf("\n");
+
+  // k++ works the same
+  while (++k < 10) {
+    printf("%d\n", k);
+  }
+
+  printf("\n");
+
+  // k-- works the same
+  while (--l >= 0) {
+    printf("%d\n", l);
   }
 
   return 0;
